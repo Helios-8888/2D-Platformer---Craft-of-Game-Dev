21010cdada6d82101c2adb22bdfa2e63d8ae0cd9
Helios-8888 <97148237+Helios-8888@users.noreply.github.com>
2025-10-13 11:31:47 +0100

    Animations
    Added idle, jump, blink and run animations
    Added transitions for idle, jump and run

--- 2D Platformer/Assets/PlayerControls.cs
+++ 2D Platformer/Assets/PlayerControls.cs
@@ -3,22 +3,35 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
     public float MoveSpeed, JumpForce, AirSpeed, AirMax;
     public bool Grounded = false;   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         Grounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Animator Controller
+        float xSpeed = Mathf.Abs(rb.linearVelocityX);
+        animator.SetFloat("xSpeed", xSpeed);
+        float ySpeed = rb.linearVelocityY;
+        animator.SetFloat("ySpeed", ySpeed);
+
+        //Get ButtonDown Has to be in update
         if (Input.GetButtonDown("Jump") && Grounded)
         {
             rb.AddForce(JumpForce * Vector2.up, ForceMode2D.Impulse);
         }
+
+        //Flips the sprite when moving in the other direction
+        if (rb.linearVelocity.x * transform.localScale.x < 0.0f) //Checks if the sprite is moving to the left
+            transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y, transform.localScale.z); 
     }
 
     private void FixedUpdate()
